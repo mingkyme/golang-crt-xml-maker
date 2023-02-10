0690680d0cd88c4c30dc12e9e203e0e0b1c9e846
MINGKYME <admin@mingky.me>
2023-02-10 14:18:07 +0900

    ✨ folder 기능 추가

--- main.go
+++ main.go
@@ -17,11 +17,24 @@ func main() {
 		panic("input.txt 파일 읽기 실패")
 	}
 	inputLineList := strings.Split(string(inputBytes), "\n")
+
 	result := ""
-	result += fmt.Sprintf(`<?xml version="1.0" encoding="UTF-8"?><VanDyke version="3.0"><key name="Sessions"><key name="%s">`, time.Now().Format("2006-01-02_15:04:05"))
-	for _, l := range inputLineList {
-		l = strings.TrimSpace(l)
-		result += fmt.Sprintf(`<key name="%s"><string name="Hostname">%s</string><dword name="[SSH2] Port">%d</dword><string name="Username">%s</string></key>`, l, l, PORT, USER)
+	result += fmt.Sprintf(`<?xml version="1.0" encoding="UTF-8"?><VanDyke version="3.0"><key name="Sessions"><key name="%s">`, time.Now().Format("2006-01-02"))
+
+	obj := make(map[string][]string)
+	for _, line := range inputLineList {
+		data := strings.Split(line, ",")
+		folderName := strings.TrimSpace(data[0])
+		serverName := strings.TrimSpace(data[1])
+		obj[folderName] = append(obj[folderName], serverName)
+	}
+
+	for key, serverList := range obj {
+		result += fmt.Sprintf(`<key name="%s">`, key)
+		for _, server := range serverList {
+			result += fmt.Sprintf(`<key name="%s"><string name="Hostname">%s</string><dword name="[SSH2] Port">%d</dword><string name="Username">%s</string></key>`, server+" "+key, server, PORT, USER)
+		}
+		result += `</key>`
 	}
 	result += `</key></key></VanDyke>`
 	err = ioutil.WriteFile("output.xml", []byte(result), 0644)
